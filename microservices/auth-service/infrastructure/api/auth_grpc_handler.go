5bff1cc7eee3a0a3706555c581c1e9e081bbc30f
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-05-04 14:16:01 +0200

    fix: renamed module name

--- microservices/auth-service/infrastructure/api/auth_grpc_handler.go
+++ microservices/auth-service/infrastructure/api/auth_grpc_handler.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	pb "module/common/proto/auth_service"
+	pb "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/auth_service"
 
 	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/auth-service/application"
 )
--- microservices/common/go.mod
+++ microservices/common/go.mod
@@ -1,4 +1,4 @@
-module common
+module github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common
 
 go 1.17
 
