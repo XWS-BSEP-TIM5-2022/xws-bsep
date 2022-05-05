4e85be6a078b86d1a64f0f3aca60785ee0e14eec
TeodoraMaruna <maruna.ra117.2018@uns.ac.rs>
2022-05-05 16:26:53 +0200

    fix: post proto files deleted

--- microservices/post-service/infrastructure/api/pb_mapper.go
+++ microservices/post-service/infrastructure/api/pb_mapper.go
@@ -1,16 +1,15 @@
 package api
 
 import (
-	pb "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/post_service"
 	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/post_service/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func mapPost(post *domain.Post) *pb.Post {
 	postPb := &pb.Post{
-		Id:          post.Id.Hex(),
-		Text:        post.Text,
-		DateCreated: post.DateCreated,
+		Id:   post.Id.Hex(),
+		Text: post.Text,
+		//DateCreated: post.DateCreated,
 	}
 	return postPb
 }
