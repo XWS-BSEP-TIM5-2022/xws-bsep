bc6e972f7dc5058814e85c7d4c7efe235bb930a8
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-05-06 21:35:38 +0200

    fix: accessible roles

--- microservices/user-service/startup/config/accessible_roles.go
+++ microservices/user-service/startup/config/accessible_roles.go
@@ -1,9 +1,9 @@
 package config
 
-// => /naziv_modula_proto_file.naziv_servisa/naziv_metode : { naziv_role1, naziv_role2 }
+// => /naziv_paketa.naziv_servisa/naziv_metode : { naziv_role1, naziv_role2 }
 // za sve metode koje ne treba da se presrecu -> ne dodaju se u mapu
 func AccessibleRoles() map[string][]string {
-	const userService = "/user.UserService/"
+	const userService = "/user_service.UserService/"
 
 	return map[string][]string{
 		userService + "Get":                 {"User"},
