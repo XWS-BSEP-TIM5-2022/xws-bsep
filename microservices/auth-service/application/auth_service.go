223bfc44005e5839f68e996507001ec43d5b4779
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-05-18 14:03:15 +0200

    refactor: moved pethods

--- microservices/auth-service/application/auth_service.go
+++ microservices/auth-service/application/auth_service.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/auth-service/domain"
 	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/auth-service/infrastructure/persistence"
-	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/auth-service/startup/config"
 	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/interceptor"
 	pb "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/auth_service"
 	user "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/user_service"
@@ -30,6 +29,143 @@ func NewAuthService(store *persistence.AuthPostgresStore, jwtService *JWTService
 		jwtService:        jwtService,
 		userServiceClient: userServiceClient,
 	}
+}
+
+func (service *AuthService) PasswordlessLogin(ctx context.Context, request *pb.PasswordlessLoginRequest) (*pb.PasswordlessLoginResponse, error) {
+	authCredentials, err := service.store.FindByUsername(request.Username)
+	if err != nil {
+		return nil, errors.New("there is no user with that username")
+	}
+
+	getUserRequest := &user.GetRequest{
+		Id: authCredentials.Id,
+	}
+
+	user, err := service.userServiceClient.GetEmail(context.TODO(), getUserRequest)
+
+	if err != nil {
+		return nil, errors.New("no user found")
+	}
+
+	from := config.NewConfig().EmailFrom
+	password := config.NewConfig().EmailPassword
+
+	to := []string{
+		user.Email,
+	}
+
+	smtpHost := config.NewConfig().EmailHost
+	smtpPort := config.NewConfig().EmailPort
+
+	token, err := service.jwtService.GenerateToken(authCredentials)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not generate JWT token")
+	}
+
+	message := passwordlessLoginMailMessage(token)
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("error while sending mail")
+	}
+
+	return &pb.PasswordlessLoginResponse{
+		Success: "Email Sent Successfully! Check your email.",
+	}, nil
+}
+
+func passwordlessLoginMailMessage(token string) []byte {
+
+	urlRedirection := "http://localhost:" + "8080" + "/api/auth/confirm-email-login/" + token
+
+	subject := "Subject: Passwordless login\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body := "<html><body style=\"background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;\">\n" +
+		"    <!-- HIDDEN PREHEADER TEXT -->\n" +
+		"    <div style=\"display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;\"> We're thrilled to have you here! Get ready to dive into your new account.\n" +
+		"    </div>\n" +
+		"    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
+		"        <!-- LOGO -->\n" +
+		"        <tr>\n" +
+		"            <td bgcolor=\"#FFA73B\" align=\"center\">\n" +
+		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+		"                    <tr>\n" +
+		"                        <td align=\"center\" valign=\"top\" style=\"padding: 40px 10px 40px 10px;\"> </td>\n" +
+		"                    </tr>\n" +
+		"                </table>\n" +
+		"            </td>\n" +
+		"        </tr>\n" +
+		"        <tr>\n" +
+		"            <td bgcolor=\"#FFA73B\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\n" +
+		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+		"                    <tr>\n" +
+		"                        <td bgcolor=\"#ffffff\" align=\"center\" valign=\"top\" style=\"padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;\">\n" +
+		"                            <h1 style=\"font-size: 48px; font-weight: 400; margin: 2;\">Dislinkt</h1> <img src=\" https://img.icons8.com/cotton/100/000000/security-checked--v3.png\" width=\"125\" height=\"120\" style=\"display: block; border: 0px;\" />\n" +
+		"                        </td>\n" +
+		"                    </tr>\n" +
+		"                </table>\n" +
+		"            </td>\n" +
+		"        </tr>\n" +
+		"        <tr>\n" +
+		"            <td bgcolor=\"#f4f4f4\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\n" +
+		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
+		"                    <tr>\n" +
+		"                        <td bgcolor=\"#ffffff\" align=\"left\" style=\"padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;\">\n" +
+		"                            <p style=\"margin: 0;\">Someone tried to sign in to your account without password. Was that you?</p>\n" +
+		"                        </td>\n" +
+		"                    </tr>\n" +
+		"                    <tr>\n" +
+		"                        <td bgcolor=\"#ffffff\" align=\"left\">\n" +
+		"                            <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n" +
+		"                                <tr>\n" +
+		"                                    <td bgcolor=\"#ffffff\" align=\"center\" style=\"padding: 20px 30px 60px 30px;\">\n" +
+		"                                        <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n" +
+		"                                            <tr>\n" +
+		"                                                <td align=\"center\" style=\"border-radius: 3px;\" bgcolor=\"#FFA73B\"><a href=\"" + urlRedirection + "\" target=\"_blank\" style=\"font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;\">Yes! Login</a></td>\n" +
+		"                                            </tr>\n" +
+		"                                        </table>\n" +
+		"                                    </td>\n" +
+		"                                </tr>\n" +
+		"                            </table>\n" +
+		"                        </td>\n" +
+		"                    </tr> \n" +
+		"    </table>\n" +
+		"    <br> <br>\n" +
+		"</body>" +
+		"</html>"
+	message := []byte(subject + mime + body)
+	return message
+}
+
+func (service *AuthService) ConfirmEmailLogin(ctx context.Context, request *pb.ConfirmEmailLoginRequest) (*pb.ConfirmEmailLoginResponse, error) {
+
+	token, err := jwt.ParseWithClaims(
+		request.Token,
+		&interceptor.UserClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodRSA)
+			if !ok {
+				return nil, fmt.Errorf("Unexpected token signing method")
+			}
+			return service.jwtService.publicKey, nil
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid token: %w", err)
+	}
+	_, ok := token.Claims.(*interceptor.UserClaims)
+	if !ok {
+		return nil, fmt.Errorf("Invalid token claims")
+	}
+
+	return &pb.ConfirmEmailLoginResponse{
+		Token: request.Token,
+	}, nil
 }
 
 func (service *AuthService) Register(ctx context.Context, request *pb.RegisterRequest) (*pb.RegisterResponse, error) {
@@ -297,142 +433,5 @@ func (service *AuthService) ChangePassword(ctx context.Context, request *pb.Chan
 	return &pb.ChangePasswordResponse{
 		StatusCode: "200",
 		Message:    "New password updated",
-	}, nil
-}
-
-func (service *AuthService) PasswordlessLogin(ctx context.Context, request *pb.PasswordlessLoginRequest) (*pb.PasswordlessLoginResponse, error) {
-	authCredentials, err := service.store.FindByUsername(request.Username)
-	if err != nil {
-		return nil, errors.New("there is no user with that username")
-	}
-
-	getUserRequest := &user.GetRequest{
-		Id: authCredentials.Id,
-	}
-
-	user, err := service.userServiceClient.GetEmail(context.TODO(), getUserRequest)
-
-	if err != nil {
-		return nil, errors.New("no user found")
-	}
-
-	from := config.NewConfig().EmailFrom
-	password := config.NewConfig().EmailPassword
-
-	to := []string{
-		user.Email,
-	}
-
-	smtpHost := config.NewConfig().EmailHost
-	smtpPort := config.NewConfig().EmailPort
-
-	token, err := service.jwtService.GenerateToken(authCredentials)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Could not generate JWT token")
-	}
-
-	message := passwordlessLoginMailMessage(token)
-
-	// Authentication.
-	auth := smtp.PlainAuth("", from, password, smtpHost)
-
-	// Sending email.
-	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println(err)
-		return nil, errors.New("error while sending mail")
-	}
-
-	return &pb.PasswordlessLoginResponse{
-		Success: "Email Sent Successfully! Check your email.",
-	}, nil
-}
-
-func passwordlessLoginMailMessage(token string) []byte {
-
-	urlRedirection := "http://localhost:" + "8080" + "/api/auth/confirm-email-login/" + token
-
-	subject := "Subject: Passwordless login\n"
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	body := "<html><body style=\"background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;\">\n" +
-		"    <!-- HIDDEN PREHEADER TEXT -->\n" +
-		"    <div style=\"display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;\"> We're thrilled to have you here! Get ready to dive into your new account.\n" +
-		"    </div>\n" +
-		"    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n" +
-		"        <!-- LOGO -->\n" +
-		"        <tr>\n" +
-		"            <td bgcolor=\"#FFA73B\" align=\"center\">\n" +
-		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
-		"                    <tr>\n" +
-		"                        <td align=\"center\" valign=\"top\" style=\"padding: 40px 10px 40px 10px;\"> </td>\n" +
-		"                    </tr>\n" +
-		"                </table>\n" +
-		"            </td>\n" +
-		"        </tr>\n" +
-		"        <tr>\n" +
-		"            <td bgcolor=\"#FFA73B\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\n" +
-		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
-		"                    <tr>\n" +
-		"                        <td bgcolor=\"#ffffff\" align=\"center\" valign=\"top\" style=\"padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;\">\n" +
-		"                            <h1 style=\"font-size: 48px; font-weight: 400; margin: 2;\">Dislinkt</h1> <img src=\" https://img.icons8.com/cotton/100/000000/security-checked--v3.png\" width=\"125\" height=\"120\" style=\"display: block; border: 0px;\" />\n" +
-		"                        </td>\n" +
-		"                    </tr>\n" +
-		"                </table>\n" +
-		"            </td>\n" +
-		"        </tr>\n" +
-		"        <tr>\n" +
-		"            <td bgcolor=\"#f4f4f4\" align=\"center\" style=\"padding: 0px 10px 0px 10px;\">\n" +
-		"                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
-		"                    <tr>\n" +
-		"                        <td bgcolor=\"#ffffff\" align=\"left\" style=\"padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;\">\n" +
-		"                            <p style=\"margin: 0;\">Someone tried to sign in to your account without password. Was that you?</p>\n" +
-		"                        </td>\n" +
-		"                    </tr>\n" +
-		"                    <tr>\n" +
-		"                        <td bgcolor=\"#ffffff\" align=\"left\">\n" +
-		"                            <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n" +
-		"                                <tr>\n" +
-		"                                    <td bgcolor=\"#ffffff\" align=\"center\" style=\"padding: 20px 30px 60px 30px;\">\n" +
-		"                                        <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n" +
-		"                                            <tr>\n" +
-		"                                                <td align=\"center\" style=\"border-radius: 3px;\" bgcolor=\"#FFA73B\"><a href=\"" + urlRedirection + "\" target=\"_blank\" style=\"font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;\">Yes! Login</a></td>\n" +
-		"                                            </tr>\n" +
-		"                                        </table>\n" +
-		"                                    </td>\n" +
-		"                                </tr>\n" +
-		"                            </table>\n" +
-		"                        </td>\n" +
-		"                    </tr> \n" +
-		"    </table>\n" +
-		"    <br> <br>\n" +
-		"</body>" +
-		"</html>"
-	message := []byte(subject + mime + body)
-	return message
-}
-
-func (service *AuthService) ConfirmEmailLogin(ctx context.Context, request *pb.ConfirmEmailLoginRequest) (*pb.ConfirmEmailLoginResponse, error) {
-
-	token, err := jwt.ParseWithClaims(
-		request.Token,
-		&interceptor.UserClaims{},
-		func(token *jwt.Token) (interface{}, error) {
-			_, ok := token.Method.(*jwt.SigningMethodRSA)
-			if !ok {
-				return nil, fmt.Errorf("Unexpected token signing method")
-			}
-			return service.jwtService.publicKey, nil
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("Invalid token: %w", err)
-	}
-	_, ok := token.Claims.(*interceptor.UserClaims)
-	if !ok {
-		return nil, fmt.Errorf("Invalid token claims")
-	}
-
-	return &pb.ConfirmEmailLoginResponse{
-		Token: request.Token,
 	}, nil
 }
