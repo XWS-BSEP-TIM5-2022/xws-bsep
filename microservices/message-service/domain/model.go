a3b4f5258dd12b755c83d37964dd3c0fe28879e4
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-06-24 02:14:04 +0200

    feat: added model

--- microservices/message-service/domain/model.go
+++ microservices/message-service/domain/model.go
@@ -2,8 +2,19 @@ package domain
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type Message struct {
-	Id primitive.ObjectID `bson:"_id"`
+	Id      primitive.ObjectID `bson:"_id"`
+	Sender  primitive.ObjectID `bson:"senderId"`
+	Content string             `bson:"content"`
+	Time    time.Time          `bson:"time"`
+}
+
+type Conversation struct {
+	Id       primitive.ObjectID `bson:"_id"`
+	User1    primitive.ObjectID `bson:"user1"`
+	User2    primitive.ObjectID `bson:"user2"`
+	Messages []Message          `bson:"messages"`
 }
