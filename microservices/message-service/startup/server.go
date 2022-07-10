77c7d91a205badf78302bce8c5bc49a6d9059f09
Kristina Stojic <stojic.kris@gmail.com>
2022-07-10 18:02:17 +0200

    feat: message service tracing

--- microservices/message-service/startup/server.go
+++ microservices/message-service/startup/server.go
@@ -6,7 +6,10 @@ import (
 	message "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/message_service"
 	notification "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/notification_service"
 	user "github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/proto/user_service"
+	"github.com/XWS-BSEP-TIM5-2022/xws-bsep/microservices/common/tracer"
+	otgo "github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
+	"io"
 	"log"
 	"net"
 
@@ -24,13 +27,22 @@ import (
 type Server struct {
 	config       *config.Config
 	CustomLogger *api.CustomLogger
+	tracer       otgo.Tracer
+	closer       io.Closer
 }
+
+const name = "message-service"
 
 func NewServer(config *config.Config) *Server {
 	CustomLogger := api.NewCustomLogger()
+	tracer, closer := tracer.Init(name)
+	otgo.SetGlobalTracer(tracer)
+
 	return &Server{
 		config:       config,
 		CustomLogger: CustomLogger,
+		tracer:       tracer,
+		closer:       closer,
 	}
 }
 
